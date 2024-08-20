be1282b864e317a256874deae457a8af8508399f
Illia Tykhonov <itykhono@c3r10s4.42warsaw.pl>
2024-08-20 18:38:11 +0200

    comments for main.c

--- main.c
+++ main.c
@@ -6,12 +6,23 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 18:04:00 by itykhono          #+#    #+#             */
-/*   Updated: 2024/08/20 18:04:41 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/08/20 18:22:55 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 //1) COPY ENV
+//2) Validate input
+//3) Lexer (String -> Tokens)
+//4) Parser (Tokens -> Logical_groups -> cmd_groups)
 int	main (int argc, char **argv)
 {
+	int	is_looping;
+
+	is_looping = 1;
+	while (is_looping == 1)
+	{
+		
+	}
 	return (0);
 }
