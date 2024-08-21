f5fdcc3d8ba716f9b3aa22e3d9b0b4f9d64aedbc
SzymonSuchanek <jus20be20+1@gmail.com>
2024-08-21 17:45:49 +0200

    adding while loop, readline, update makefile and header, add function lib_ft/strndup.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 18:04:00 by itykhono          #+#    #+#             */
-/*   Updated: 2024/08/21 15:39:23 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/08/21 17:29:00 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,20 @@
 
 #include "minishell.h"
 
+void	minishell_loop(t_data *minishell)
+{
+	while (1)
+	{
+		minishell->input = readline(PROMPT);
+	}
+}
+
 void	init_minishell(t_data *minishell, char **env)
 {
 	minishell->envir = env;
+	minishell->stdin = dup(0);
+	minishell->stdout = dup(1);
+	tcgetattr(STDIN_FILENO, &minishell->terminal);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -28,10 +39,8 @@ int	main(int argc, char **argv, char **env)
 
 	init_minishell(&minishell, env);
 	init_environment(&minishell.env, minishell.envir);
-	print_environment(minishell.env);
+	// print_environment(minishell.env);
 	environment_free_list(minishell.env);
-	// while (1)
-	// {
-	// }
+	minishell_loop(&minishell);
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 21:48:08 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/21 15:37:42 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/08/21 17:28:52 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 # define MINISHELL_H
 
 # include "./lib_ft/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdbool.h>
 # include <stdlib.h>
+# include <termios.h>
 # include <unistd.h>
+
+# define PROMPT "MDshell > "
 
 // DRBUG_FIELD
 // DELETE ME befor release
@@ -32,11 +37,17 @@ typedef struct s_env
 
 typedef struct s_data
 {
+	char				*input;
 	char				**envir;
 	char				*environment;
-	struct s_minishell	*next;
+	int					stdin;
+	int					stdout;
 	t_env				*env;
+	struct termios		terminal;
+	struct s_minishell	*next;
 }						t_data;
+
+void					minishell_loop(t_data *minishell);
 
 t_env					*environment_new_node(char *key, char *value);
 void					environment_new_node_end(t_env **head, char *key,
