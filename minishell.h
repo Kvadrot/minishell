46a59f1d7d9ced79b17842ae4f65c82b7627771d
Illia Tykhonov <itykhono@c3r10s1.42warsaw.pl>
2024-08-22 14:09:25 +0200

    ft_is_valid input is done

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 21:48:08 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/22 13:20:38 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/08/22 13:36:16 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,9 @@ typedef struct s_data
 	struct termios		terminal;
 	struct s_minishell	*next;
 }						t_data;
+
+// Validate_input
+bool	ft_input_is_valid(char *input_str);
 
 void					minishell_loop(t_data *minishell);
 
