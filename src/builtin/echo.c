df47f806a278ee10967057075930e4f9a5f72532
SzymonSuchanek <jus20be20+1@gmail.com>
2024-09-07 20:14:13 +0200

    update

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 17:23:52 by ssuchane          #+#    #+#             */
-/*   Updated: 2024/09/07 20:04:16 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/09/07 20:14:04 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 typedef struct s_execcmd
 {
-	int				type;
-	char			*argv[MAXARGS];
-	char			*eargv[MAXARGS];
-	char			*paths;
-	bool			flag;
-}					t_execcmd;
+	int		type;
+	char	*argv[MAXARGS];
+	char	*eargv[MAXARGS];
+	char	*paths;
+	char	*flag;
+}			t_execcmd;
 
 char	**get_paths(char *path_env)
 {
 	int		paths_count;
 	int		i;
 	char	**paths;
-	char	*path_copy;
 
 	if (path_env == NULL)
 		return (NULL);
@@ -38,36 +37,31 @@ char	**get_paths(char *path_env)
 	paths = malloc((paths_count + 1) * sizeof(char *));
 	if (paths == NULL)
 		return (NULL);
-	path_copy = strdup(path_env);
-	if (path_copy == NULL)
-	{
-		free(paths);
-		return (NULL);
-	}
-	paths = ft_split(path_copy, ":");
-	free(path_copy);
+	paths = ft_split(path_env, ":");
 	return (paths);
 }
 
 bool	check_builtin(t_cmd *execcmd, char **paths)
 {
-	int i = 0;
-	char *full_path = NULL;
-	int path_len;
-	int cmd_len;
+	int		i;
+	char	*full_path;
+	int		path_len;
+	int		cmd_len;
 
-	if (cmd == NULL || cmd->value == NULL || paths == NULL)
+	i = 0;
+	full_path = NULL;
+	if (cmd == NULL || cmd->argv[0] == NULL || paths == NULL)
 		return (false);
 	while (paths[i] != NULL)
 	{
 		path_len = strlen(paths[i]);
-		cmd_len = strlen(cmd->value);
+		cmd_len = strlen(cmd->argv[0]);
 		full_path = malloc(path_len + 1 + cmd_len + 1);
 		if (full_path == NULL)
 			return (false);
 		strcpy(full_path, paths[i]);
 		strcat(full_path, "/");
-		strcat(full_path, cmd->value);
+		strcat(full_path, cmd->argv[0]);
 		if (access(full_path, X_OK) == 0)
 		{
 			free(full_path);
@@ -76,6 +70,7 @@ bool	check_builtin(t_cmd *execcmd, char **paths)
 		free(full_path);
 		i++;
 	}
+	return (false);
 }
 
 int	check_flags(t_execcmd builtin, bool *new_line)
@@ -124,10 +119,10 @@ int	do_echo(t_execcmd builtin)
 	return (status);
 }
 
-void do_builtin(t_execcmd * builtins)
+void	do_builtin(t_execcmd *builtins)
 {
-	// before getting into this function check if the command exists with /path
-	// use function check_builtin 
+	// before getting into this function check if the command exists
+	// use function check_builtin();
 	if (builtins->type == ECHO)
 		do_echo(builtins);
 	else if (builtins->type == CD)
