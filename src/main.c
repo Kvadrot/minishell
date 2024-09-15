aeb2fb1a384c9cba36c6b8034b1fb550268d14f1
SzymonSuchanek <jus20be20+1@gmail.com>
2024-09-15 16:39:28 +0200

    typo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ssuchane <ssuchane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:51:34 by gbuczyns          #+#    #+#             */
-/*   Updated: 2024/09/15 16:38:38 by ssuchane         ###   ########.fr       */
+/*   Updated: 2024/09/15 16:39:20 by ssuchane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,18 @@
 
 #include "../inc/minishell.h"
 
-void handle_sigint(int sig)
+void	handle_sigint(int sig)
 {
-    (void)sig;
-    write(STDOUT_FILENO, "\n", 1);
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-void minishell_loop(t_data *minishell)
+void	minishell_loop(t_data *minishell)
 {
 	// char *buff;
-
 	while (1)
 	{
 		// minishell->tokens = NULL;
@@ -53,18 +52,14 @@ void minishell_loop(t_data *minishell)
 		alloc_mem_for_commands(minishell);
 		parsecmd(minishell);
 		run_with_pipes(minishell);
-		
 		// free_global(minishell);
 		minishell->input = NULL;
 		minishell->pipe_argv = NULL;
 		minishell->number_of_commands = 0;
 		minishell->commands = NULL;
-		
 		// char buff[] = "echo tav > ztest"; //>> tak.txt | ls > ls";
 		// printf("after prompt\n");
-		
 		// char buff[] = "echo tav > ztest"; //>> tak.txt | ls > ls";
-
 		// runcmd(parsecmd(buff));
 		// init_tokens(minishell);
 		// check_syntax(minishell->tokens);
@@ -78,7 +73,8 @@ void minishell_loop(t_data *minishell)
 		// }
 		// seg fault here (no input given)
 		// ft_input_is_valid(minishell->input);
-		// if (minishell->commands && minishell->commands->args[0]) // cd /nfs/homes/gbuczyns/Documents/CommonCore/level_4 >> asdas
+		// if (minishell->commands && minishell->commands->args[0]) // cd
+			/nfs/homes/gbuczyns/Documents/CommonCore/level_4 >> asdas
 		// {
 		// 	buff = minishell->commands->args[0];
 		// 	if (ft_strcmp(buff, "cd") == 0)
@@ -88,15 +84,14 @@ void minishell_loop(t_data *minishell)
 	}
 }
 
-
-void init_minishell(t_data *minishell, char **env)
+void	init_minishell(t_data *minishell, char **env)
 {
 	minishell->envir = env;
 	tcgetattr(STDIN_FILENO, &minishell->terminal);
 	minishell->number_of_commands = 0;
 }
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	t_data	minishell;
 
@@ -104,7 +99,6 @@ int main(int argc, char **argv, char **env)
 	(void)argv;
 	(void)env;
 	// char buff[] = "echo tav > test"; //>> tak.txt | ls > ls";
-
 	signal(SIGINT, handle_sigint);
 	init_minishell(&minishell, env);
 	init_environment(&minishell.envlist, minishell.envir);
