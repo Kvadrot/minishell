2ec114b9c2f5bdbdb22adc070d99afcebeba26bd
Kvadrot <itykhono@c3r8s3.42warsaw.pl>
2024-10-01 14:07:06 +0200

    printf with custom output fd is ready

--- lib_ft_printf/ft_printf.c
+++ lib_ft_printf/ft_printf.c
@@ -6,38 +6,38 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 12:21:02 by itykhono          #+#    #+#             */
-/*   Updated: 2024/08/05 23:16:41 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/10/01 14:06:04 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_put_chr(char ch)
+int	ft_put_chr(char ch, int fd_output)
 {
-	write(1, &ch, 1);
+	write(1, &ch, fd_output);
 	return (1);
 }
 
-int	ft_detect_param(va_list args, char ch)
+int	ft_detect_param(va_list args, char ch, int output_fd)
 {
 	if (ch == 'c')
-		return (ft_put_chr((char)va_arg(args, int)));
+		return (ft_put_chr((char)va_arg(args, int), output_fd));
 	else if (ch == 's')
-		return (ft_put_str((char *)va_arg(args, char *)));
+		return (ft_put_chr((char)va_arg(args, int), output_fd));
 	else if (ch == 'd')
-		return (ft_putnbr(va_arg(args, int)));
+		return (ft_put_chr((char)va_arg(args, int), output_fd));
 	else if (ch == 'i')
-		return (ft_putnbr(va_arg(args, int)));
+		return (ft_putnbr(va_arg(args, int), output_fd));
 	else if (ch == 'u')
-		return (ft_unsigned_putnbr((int unsigned)va_arg(args, int unsigned)));
+		return (ft_unsigned_putnbr((int unsigned)va_arg(args, int unsigned), output_fd));
 	else if (ch == 'x')
-		return (ft_print_16_base((int unsigned)va_arg(args, int unsigned), 0));
+		return (ft_print_16_base((int unsigned)va_arg(args, int unsigned), 0, output_fd));
 	else if (ch == 'X')
-		return (ft_print_16_base((int unsigned)va_arg(args, int unsigned), 1));
+		return (ft_print_16_base((int unsigned)va_arg(args, int unsigned), 1, output_fd));
 	else if (ch == '%')
-		return (ft_put_chr('%'));
+		return (ft_put_chr('%', output_fd));
 	else if (ch == 'p')
-		return (ft_print_ptr(va_arg(args, void *), "0123456789abcdef"));
+		return (ft_print_ptr(va_arg(args, void *), "0123456789abcdef", output_fd));
 	else
 		return (0);
 }
@@ -58,7 +58,7 @@ int	ft_is_param_letter(char ch)
 	return (-404);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printf_full(int output_fd, const char *format, ...)
 {
 	int				i;
 	int long long	sym_sum;
@@ -74,15 +74,24 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%' && ft_is_param_letter(format[i + 1]) == 200)
 		{
 			i++;
-			sym_sum += ft_detect_param(args, format[i]);
+			sym_sum += ft_detect_param(args, format[i], output_fd);
 		}
 		else
 		{
-			ft_put_chr(format[i]);
+			ft_put_chr(format[i], output_fd);
 			sym_sum++;
 		}
 		i++;
 	}
 	va_end(args);
 	return (sym_sum);
+}
+
+int ft_printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int result = ft_printf_full(1, format, args);
+    va_end(args);
+    return result;
 }
--- lib_ft_printf/ft_printf.h
+++ lib_ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 12:20:55 by itykhono          #+#    #+#             */
-/*   Updated: 2024/07/27 19:15:35 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/10/01 13:54:15 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@
 # include <unistd.h>
 # include <stdint.h>
 
-int	ft_printf(const char *format, ...);
-int	ft_putnbr(int n);
-int	ft_put_chr(char c);
-int	ft_put_str(char *str);
-int	ft_unsigned_putnbr(int unsigned n);
+int ft_printf(const char *format, ...);
+
+
+int	ft_detect_param(va_list args, char ch, int output_fd);
+int	ft_put_chr(char ch, int fd_output);
+int	ft_put_str(char *str, int output_fd);
 int	ft_is_param_letter(char ch);
-int	ft_detect_param(va_list args, char ch);
-int	ft_print_ptr(void *ptr, char *base);
-int	ft_print_16_base(int unsigned n, int capitalized);
+int	ft_putnbr(int n, int output_fd);
+int	ft_unsigned_putnbr(int unsigned n, int output_fd);
+int	ft_print_16_base(int unsigned n, int capitalized, int output_fd);
+int	ft_print_ptr(void *ptr, char *base, int output_fd);
 
 #endif
