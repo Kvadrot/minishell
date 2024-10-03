444b99d3de7a67d0b2c4c6d75f84a389187ccebe
Illia <ufo@Illias-MacBook-Pro.local>
2024-10-03 15:58:43 +0200

    restructurisation for parsing.c

--- src/common_tools/common_tools.h
+++ src/common_tools/common_tools.h
@@ -6,7 +6,7 @@
 /*   By: ufo <ufo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 15:34:37 by ufo               #+#    #+#             */
-/*   Updated: 2024/10/03 15:39:55 by ufo              ###   ########.fr       */
+/*   Updated: 2024/10/03 15:52:32 by ufo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,5 @@
 
 int     ft_get_size_arr_of_str(char **arr_of_str);
 char    **append_string_to_array(char *new_str, char **args);
+char    *ft_join_with_delimeter(char *s1, char *s2, char *delimiter);
+
