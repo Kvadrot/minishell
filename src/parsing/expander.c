2b24461a24bf2ab1ff74a87449e09987c4edddef
Kvadrot <send.me.your.maild@gmail.com>
2024-10-17 16:55:46 +0200

    successfull merge parsing into dev

--- src/parsing/expander.c
+++ src/parsing/expander.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ufo <ufo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:50:28 by ufo               #+#    #+#             */
-/*   Updated: 2024/10/16 15:53:03 by ufo              ###   ########.fr       */
+/*   Updated: 2024/10/17 16:55:34 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+/** TODO: ft_check_is_able_to_substitude
+* @brief: checks wether super quetes are not single quotes''
+* @takes: string - temp argument
+//=======================================================================//
+* @HOW_IT_works:
+1) checks for opened quotes before and identify its type
+//=======================================================================//
+* @returns: Bool
+*/
+bool ft_check_is_able_to_substitude(char *str_argument)
+{
+	// if 
+	return (true);
+}
 
 /** TODO: ft_might_be_substituted
 * @brief: decides should we change unwrap the $ARG or not
@@ -24,7 +38,20 @@
 */
 bool ft_might_be_substituted(char **super_arr, char *temp_arg)
 {
-    // if (str)
+	char	super_quote; // " somethingkagjjg jwog;erogh 'test'  " super quote is ""
+	char	temp_pointer;
+	bool	is_able_to_substitude;
+
+	temp_pointer = temp_arg;
+    while (ft_strchr(temp_pointer, '$'))
+	{
+		is_able_to_substitude = ft_check_is_able_to_substitude(temp_pointer);
+		temp_pointer = ft_strchr(temp_pointer, '$');
+		
+		
+		// temp_pointer = 
+	}
+
     return (false);
 }
 
