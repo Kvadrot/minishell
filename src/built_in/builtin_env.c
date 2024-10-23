dbbc6953fb18537d3e0e6c4199ab2ed09c7e54b4
MaryTim <iammariatimoshenko@gmail.com>
2024-10-23 15:34:32 +0200

    small changes

--- src/built_in/builtin_env.c
+++ src/built_in/builtin_env.c
@@ -6,15 +6,13 @@
 /*   By: mbudkevi <mbudkevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:25:18 by mbudkevi          #+#    #+#             */
-/*   Updated: 2024/10/12 13:20:31 by mbudkevi         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:24:21 by mbudkevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-// with no options or args
-
-int		builtin_env(t_env *node)
+int	builtin_env(t_env *node)
 {
 	t_env	*current;
 
--- src/built_in/handle_builtins.c
+++ src/built_in/handle_builtins.c
@@ -6,7 +6,7 @@
 /*   By: mbudkevi <mbudkevi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 17:56:21 by mbudkevi          #+#    #+#             */
-/*   Updated: 2024/10/16 09:22:47 by mbudkevi         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:29:58 by mbudkevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ bool	is_builtin(t_command_full cmd)
 {
 	char		*name;
 	int			i;
-	const char	*builtins[] = {"cd", "echo", "env", "exit", "export", "pwd", "unset"};
+	const char	*builtins[] = {"cd", "echo",
+		"env", "exit", "export", "pwd", "unset"};
 
 	i = 0;
 	name = cmd.args[0];
@@ -31,18 +32,25 @@ bool	is_builtin(t_command_full cmd)
 
 void	handle_builtins(t_data **minishell)
 {
-	if (ft_strncmp((*minishell)->commands->cmd_name, "echo", ft_strlen("echo")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "echo",
+			ft_strlen("echo")) == 0)
 		builtin_echo((*minishell)->commands->args);
-	if (ft_strncmp((*minishell)->commands->cmd_name, "cd", ft_strlen("cd")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "cd",
+			ft_strlen("cd")) == 0)
 		builtin_cd(minishell);
-	if (ft_strncmp((*minishell)->commands->cmd_name, "env", ft_strlen("env")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "env",
+			ft_strlen("env")) == 0)
 		builtin_env((*minishell)->env);
-	if (ft_strncmp((*minishell)->commands->cmd_name, "pwd", ft_strlen("pwd")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "pwd",
+			ft_strlen("pwd")) == 0)
 		builtin_pwd();
-	if (ft_strncmp((*minishell)->commands->cmd_name, "export", ft_strlen("export")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "export",
+			ft_strlen("export")) == 0)
 		builtin_export(minishell);
-	if (ft_strncmp((*minishell)->commands->cmd_name, "unset", ft_strlen("unset")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "unset",
+			ft_strlen("unset")) == 0)
 		builtin_unset(minishell);
-	if (ft_strncmp((*minishell)->commands->cmd_name, "exit", ft_strlen("exit")) == 0)
+	if (ft_strncmp((*minishell)->commands->cmd_name, "exit",
+			ft_strlen("exit")) == 0)
 		builtin_exit();
 }
